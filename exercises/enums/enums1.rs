7f26c771bbff4a0f4dfc34ba11b1b0cdd3b290cf
schm00g <hk8igureu@relay.firefox.com>
2022-01-07 21:00:07 +0000

    enums

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -1,16 +1,24 @@
 // enums1.rs
 // Make me compile! Execute `rustlings hint enums1` for hints!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit(String),
+    Echo(String),
+    Move(String),
+    ChangeColor(String)
 }
 
+
 fn main() {
-    println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::ChangeColor);
+    let quit = Message::Quit(String::from("127.0.0.1"));
+    let echo = Message::Quit(String::from("127.0.0.1"));
+    let move_ = Message::Quit(String::from("127.0.0.1"));
+    let change_color = Message::Quit(String::from("127.0.0.1"));
+
+    println!("{:?}", quit);
+    println!("{:?}", echo);
+    println!("{:?}", move_);
+    println!("{:?}", change_color);
 }
